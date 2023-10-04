02040ef00b5048267141ccaf6aae104c51972444
Emily Franco <xNovaPsyche@gmail.com>
2023-10-03 19:55:57 -0600

    Created UFO Defender Game

---
+++ LabProject/UFO Defense Force Game 2023/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+
+    public float speed = 25;
+
+    public float xRange = 25;
+
+    public Transform blaster;
+
+    public GameObject lazerBolt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set horizontalInput to recieve values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // moves player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // Keep player within bounds
+        // Left side wall
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // Right side wall
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        // If spacebar is pressed fire laserbolt
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Create laserBolt at the blaster transform position maintaining objects position
+            Instantiate(lazerBolt, blaster.transform.position, lazerBolt.transform.rotation);
+        }
+    }
+
+    // Delete any object with a trigger that hits the player
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
