cdd2985ae325d05a3619dfa52a9d6baaac484fe0
Emily Franco <xNovaPsyche@gmail.com>
2023-10-05 21:01:42 -0600

    Made Coin Script for Challenge!

---
+++ LabProject/Assets/Scripts/Collectible.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public static event Action OnCollected;
+    
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(90f, Time.time * 100f, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnCollected?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
