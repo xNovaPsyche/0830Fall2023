1c196d08454e5fedce63fb782894a428fabd954c
Emily Franco <xNovaPsyche@gmail.com>
2023-10-23 22:42:03 -0600

    Float Data Trial and Error

---
+++ LabProject/ScriptableObject/Assets/Scenes/TextLabelBehaviour.cs
@@ -0,0 +1,17 @@
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class TextLabelBehaviour : MonoBehaviour
+{
+    public Text label;
+    public FloatData dataObj;
+
+    private void Start()
+    {
+        label = GetComponent<Text>();
+        label.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+    }
+}
