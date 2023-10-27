8f1205dcc88ec2a4c7ed1d595e1f240f9c55d346
Emily Franco <xNovaPsyche@gmail.com>
2023-10-26 19:01:00 -0600

    Challenge isn't working

---
+++ LabProject/ScriptableObject/Assets/Scenes/HealthPowerUp.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class HealthPowerUp : MonoBehaviour
+{
+    public float value;
+
+    [SerializeField] private FloatData healthValue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Apply the health value to the player
+            ApplyHealth(other.gameObject);
+
+            // Destroy the power-up game object
+            Destroy(gameObject);
+        }
+    }
+
+    private void ApplyHealth(GameObject player)
+    {
+        // Retrieve the player's health component (e.g., HealthController script)
+        HealthController healthController = player.GetComponent<HealthController>();
+
+        // Check if the player has a health component
+        if (healthController != null)
+        {
+            // Add the health value to the player's current health
+            healthController.AddHealth(healthValue.Value);
+        }
+
+    public void UpdateValue(float num)
+        {
+            value += num;
+        }
+    }
+}
