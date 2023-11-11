6481ba61f42ed9bf215133b87014fd43ab3a412e
Emily Franco <xNovaPsyche@gmail.com>
2023-11-10 20:51:03 -0700

    Updated
    Animation scripts aren't exactly working but managed  to implement them without errors.

--- LabProject/Color Match Game/Assets/DraggableBehaviour.cs
+++ LabProject/Color Match Game/Assets/DraggableBehaviour.cs
@@ -1,11 +1,13 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DraggableBehaviour : MonoBehaviour
 {
     private Camera cameraObj;
     public bool draggable;
+    public Vector3 position, offset;
+    public UnityEvent startDragEvent, endDragEvent;
 
     void Start()
     {
@@ -14,12 +16,15 @@ public class DraggableBehaviour : MonoBehaviour
 
     public IEnumerator OnMouseDown()
     {
+        offset = transform.position - cameraObj.ScreenToWorldPoint(Input.mousePosition);
         draggable = true;
+        yield return new WaitForFixedUpdate();
 
         while (draggable)
         {
             yield return new WaitForFixedUpdate();
-            Debug.Log("Drag");
+            position = cameraObj.ScreenToWorldPoint(Input.mousePosition) + offset;
+            transform.position = position;
         }
     }
 
@@ -27,5 +32,6 @@ public class DraggableBehaviour : MonoBehaviour
     private void OnMouseUp()
     {
         draggable = false;
+        endDragEvent.Invoke();
     }
 }
